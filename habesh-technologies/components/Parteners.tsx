1f2311b2562a692e09995507e0cade4d2001caae
Muse-feta <fetamuse@gmail.com>
2025-10-06 19:16:12 +0300

    minor change

--- habesh-technologies/components/Parteners.tsx
+++ habesh-technologies/components/Parteners.tsx
@@ -3,42 +3,6 @@ import React from "react";
 type Props = {};
 
 const Parteners = (props: Props) => {
-  // Partner data: image + link
-  const partners = [
-    {
-      src: "assets/img/habesha/customer-1.png",
-      link: "https://mamigaspharmaceuticals.com/",
-    },
-    {
-      src: "assets/img/habesha/customer-2.png",
-      link: "https://sharjahelevator.com/",
-    },
-    {
-      src: "assets/img/habesha/customer-3.png",
-      link: "http://greenpowersystemsplc.com/",
-    },
-    {
-      src: "assets/img/habesha/customer-4.png",
-      link: "https://www.packerrdj.com/",
-    },
-    {
-      src: "assets/img/habesha/customer-1.png",
-      link: "https://mamigaspharmaceuticals.com/",
-    },
-    {
-      src: "assets/img/habesha/customer-2.png",
-      link: "https://sharjahelevator.com/",
-    },
-    {
-      src: "assets/img/habesha/customer-3.png",
-      link: "http://greenpowersystemsplc.com/",
-    },
-    {
-      src: "assets/img/habesha/customer-4.png",
-      link: "https://www.packerrdj.com/",
-    },
-  ];
-
   return (
     <div>
       <div className="tp-brand-area tp-brand-spacing tp-bg-common-white z-index-1 p-relative">
@@ -52,8 +16,41 @@ const Parteners = (props: Props) => {
 
         <div className="tp-brand-wrap">
           <div className="swiper-container tp-brand-slide-active">
-            <div className="swiper-wrapper slide-transtion flex items-center justify-center gap-6 flex-wrap">
-              {partners.map((partner, i) => (
+            <div className="swiper-wrapper slide-transtion flex items-center justify-center gap-6">
+              {[
+                {
+                  src: "assets/img/habesha/customer-1.png",
+                  link: "https://mamigaspharmaceuticals.com/",
+                },
+                {
+                  src: "assets/img/habesha/customer-2.png",
+                  link: "https://sharjahelevator.com/",
+                },
+                {
+                  src: "assets/img/habesha/customer-3.png",
+                  link: "http://greenpowersystemsplc.com/",
+                },
+                {
+                  src: "assets/img/habesha/customer-4.png",
+                  link: "https://www.packerrdj.com/",
+                },
+                {
+                  src: "assets/img/habesha/customer-1.png",
+                  link: "https://mamigaspharmaceuticals.com/",
+                },
+                {
+                  src: "assets/img/habesha/customer-2.png",
+                  link: "https://sharjahelevator.com/",
+                },
+                {
+                  src: "assets/img/habesha/customer-3.png",
+                  link: "http://greenpowersystemsplc.com/",
+                },
+                {
+                  src: "assets/img/habesha/customer-4.png",
+                  link: "https://www.packerrdj.com/",
+                },
+              ].map((partner, i) => (
                 <div key={i} className="swiper-slide">
                   <div className="tp-brand-item flex items-center justify-center">
                     <a
@@ -64,7 +61,7 @@ const Parteners = (props: Props) => {
                       <img
                         src={partner.src}
                         alt={`Client ${i + 1}`}
-                        className="w-40 sm:w-44 md:w-48 lg:w-52 object-contain transition-transform duration-300 hover:scale-105"
+                        className="w-70 h-30 sm:w-40 md:w-44 lg:w-48 object-contain"
                       />
                     </a>
                   </div>
